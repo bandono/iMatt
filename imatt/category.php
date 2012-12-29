2afebae09808b05109045bc3f57b553ae95377e3
Arif Kusbandono <arif.working@gmail.com>
2012-12-30 00:39:13 +0700

    v2.0.0 using Template for Page not working properly, pics not loading The Wordpress Loop, pics is removed attempt to use category instead

--- imatt/category.php
+++ imatt/category.php
@@ -1,10 +1,11 @@
-<?php /*
-Template Name: Pictoresque Responsive Gallery
-*/ ?>
-
 <?php get_header(); ?>
 <!-- End the general wrapper after header -->
 </div><!--/wrapper -->
+
+<!-- Load the masonry library and script -->
+<script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+
 
 <div id="container" class="fix">
     <div id="shakenmaincontent">
@@ -26,23 +27,20 @@ Template Name: Pictoresque Responsive Gallery
                         <br />
                         <?php the_tags( __( 'Tags: ' ), ', ', ' ' ); ?>
                         </div><!-- End metabar -->
-                    </div><!-- End shakenpostwrap -->
+                    </div><!-- End copy fix -->
                     <?php the_content( __( '(More ...)' ) ); ?>
                     <?php if ( get_post_meta($post->ID, 'image', true) ) : ?>
                         <div id="embeddedmeta"><?php echo get_post_meta($post->ID, 'image', true) ?></div>
                     <?php endif; ?>
                     <div class="hl"></div>
-                    <div class="post-footer">
+                    <div class="post-footer"></div>
                 </div><!-- End shakenpostwrap -->
                 <?php endwhile; ?><!-- End the Wordpress Loop -->
             <?php endif; ?>
-        </div><!-- End sort masoned -->
+        </div><!-- End /sort -->
     </div><!-- End shakenmaincontent -->
 </div> <!-- End container -->
 
-<!-- Load the masonry library and script -->
-<script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
 
 <?php if ( !is_single() ) : ?>
 <div class="page-nav fix">
