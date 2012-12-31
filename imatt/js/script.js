a5fa560aab2597bb07e8721eb4321364859686c9
Arif Kusbandono <arif.working@gmail.com>
2012-12-31 09:21:02 +0700

    v2.0.1 add resizing of image when loading masonry

--- imatt/js/script.js
+++ imatt/js/script.js
@@ -1,20 +1,52 @@
 jQuery.noConflict();
 (function($) {
 
-	$('span.view-large').hide();
+	
 	myOp = 1;
  
 	$(window).load(function(){
-		$('#sort').masonry({ 
+		$('#sort').masonry({
 			columnWidth: 280,
 			animate: true,
 			itemSelector: '.shakenpostwrap'
-		}, 
+		},
 		function() { $(this).css({
 			margin: '10px'
 			});
-		}).masonry('reload');
+		}).masonry('reload'),
+        $('.shakenpostwrap img').each(function(){
+          var maxWidth = 240;
+          var ratio = 0;
+          var img = $(this);
+          if(img.width() > maxWidth){
+            ratio = img.height() / img.width();
+            img.attr('width', maxWidth);
+            img.attr('height', (maxWidth*ratio));
+          	}
+        }).masonry('reload');
 	});
+ 
+ 
+ 
+ // MouseOver Events
+ $('.shakenpostwrap').hover(function(){
+                            $('img', this).fadeTo("fast", 0.75).addClass('shakenpostwrap-hover');
+                            $('span.view-large', this).fadeTo("fast", 1)},
+                            function(){
+                            $('img', this).fadeTo("fast", myOp).removeClass('shakenpostwrap-hover');
+                            $('span.view-large', this).fadeTo("fast", 0),
+                            function(){
+                            $('#sort').masonry({
+                                               columnWidth: 280,
+                                               animate: true,
+                                               itemSelector: '.shakenpostwrap'
+                                               },
+                                               function() { $(this).css({
+                                                                        margin: '10px'
+                                                                        });
+                                               }).masonry('reload');
+                            }
+                            });	
  
 
 	
