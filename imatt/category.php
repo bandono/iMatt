f9b3a414352f02255246b8a86a67f9af811ebe74
Arif Kusbandono <arif.working@gmail.com>
2013-01-11 16:18:03 +0700

    v2.1.1 apply previous-next style span to pagination where category.php doesn't call footer.php

--- imatt/category.php
+++ imatt/category.php
@@ -63,11 +63,19 @@
     </div><!-- End shakenmaincontent -->
 </div> <!-- End container -->
 
-
 <?php if ( !is_single() ) : ?>
-<div class="page-nav fix">
-    <?php posts_nav_link(); ?>
-</div><!-- page nav -->
+    <div class="page-nav fix">
+        <?php if(function_exists('wp_pagenavi')) : ?>
+            <?php wp_pagenavi(); ?>
+        <?php else : ?>
+            <span class="previous-entries">
+                <?php next_posts_link(__('Older Entries', get_option('posts_per_page'))); ?>
+            </span>
+            <span class="next-entries">
+                <?php previous_posts_link(__('Newer Entries', get_option('posts_per_page'))); ?>
+            </span>
+        <?php endif; ?>
+    	</div><!-- page nav -->
 <?php endif; ?>
 
 </div><!--/page -->
