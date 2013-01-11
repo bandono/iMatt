0104e3c21c59f568ca41222cea667ef8e0876e0e
Arif Kusbandono <arif.working@gmail.com>
2013-01-11 16:17:08 +0700

    v2.1.1 apply previous-next style span to pagination where all: index.php, author.php, tag.php, page.php use it

--- imatt/footer.php
+++ imatt/footer.php
@@ -1,10 +1,19 @@
 <?php get_sidebar( ); ?>
 
-<?php if ( !is_single() ) : ?>
-	<div class="page-nav fix"> 
-		<?php posts_nav_link(); ?> 
-	</div><!-- page nav --> 
-<?php endif; ?>
+    <?php if ( !is_single() ) : ?>
+        <div class="page-nav fix">
+        <?php if(function_exists('wp_pagenavi')) : ?>
+            <?php wp_pagenavi(); ?>
+        <?php else : ?>
+            <span class="previous-entries">
+                <?php next_posts_link(__('Older Entries', get_option('posts_per_page'))); ?>
+            </span>
+            <span class="next-entries">
+                <?php previous_posts_link(__('Newer Entries', get_option('posts_per_page'))); ?>
+            </span>
+        <?php endif; ?>
+        </div><!-- page nav -->
+    <?php endif; ?>
 	</div><!--/wrapper -->
 </div><!--/page -->
 </body>
